32c36f6aba8a5522465ea180707e93ae62460658
Joe Maples <joe@frap129.org>
2017-01-17 12:41:53 -0500

    cpufreq: add relaxed governor
    Relaxed is a governor based on chill, but impliments a boost_ceiling parameter that allows for more gradual boosting. With chill, I noticed that the min and max frequencies were being used the most, and all other frequencies were being barely used at all. Rather than boosting straight to the max frequency, relaxed finds the differenced between boost_counter and boost_ceiling, then boosts to max minus that difference. For example, if boost_count is 8 and boost_ceiling is 12, when boost_counter reaches 8, the cpu freq will boost to 4 (12-8) levels below max, then increase if needed untill boost_counter reaches boost_ceiling. This governor doesn't completely replace chill, but is intended to be used alongside it. Since the lowest frequency on a57 cores uses more power than the highest freq on a53 cores, it is still valuable to use chill on a57 in order to complete a task as quickly as possible.
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -465,6 +465,9 @@ extern struct cpufreq_governor cpufreq_gov_chill;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
 extern struct cpufreq_governor cpufreq_gov_cafactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_RELAXED)
+extern struct cpufreq_governor cpufreq_gov_relaxed;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_relaxed)
 #endif
 
 /*********************************************************************
