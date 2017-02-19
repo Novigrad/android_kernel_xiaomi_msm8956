2860180b34f2894fb28d2ff6dd1d1d81fc2821c5
Akuma70 <mohammad.parsa1370@gmail.com>
2017-02-19 13:08:51 +0330

    ft5x06_ts: show touchscreen info and firmware

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -45,6 +45,8 @@
 #include <linux/fastcharge.h>
 #endif
 
+int FG_charger_status = 0;
+
 /* Mask/Bit helpers */
 #define _SMB_MASK(BITS, POS) \
 	((unsigned char)(((1 << (BITS)) - 1) << (POS)))
@@ -4189,8 +4191,9 @@ reschedule:
 static int smbchg_charging_status_change(struct smbchg_chip *chip)
 {
 	smbchg_vfloat_adjust_check(chip);
+	FG_charger_status = get_prop_batt_status(chip);
 	set_property_on_fg(chip, POWER_SUPPLY_PROP_STATUS,
-			get_prop_batt_status(chip));
+			FG_charger_status);
 	return 0;
 }
 
@@ -4644,8 +4647,9 @@ void update_usb_status(struct smbchg_chip *chip, bool usb_present, bool force)
 	}
 
 	/* update FG */
+	FG_charger_status = get_prop_batt_status(chip);
 	set_property_on_fg(chip, POWER_SUPPLY_PROP_STATUS,
-			get_prop_batt_status(chip));
+			FG_charger_status);
 unlock:
 	mutex_unlock(&chip->usb_status_lock);
 }
@@ -6673,8 +6677,9 @@ static irqreturn_t usbid_change_handler(int irq, void *_chip)
 		pr_smb(PR_STATUS, "OTG detected\n");
 
 	/* update FG */
+	FG_charger_status = get_prop_batt_status(chip);
 	set_property_on_fg(chip, POWER_SUPPLY_PROP_STATUS,
-			get_prop_batt_status(chip));
+			FG_charger_status);
 
 	return IRQ_HANDLED;
 }
