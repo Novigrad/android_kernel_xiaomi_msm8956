cccec289594b940417cd4bbb2d5d10b77eb2c78c
Akuma70 <mohammad.parsa1370@gmail.com>
2017-03-18 21:36:38 +0330

    Revert "mm: zcache: don't use Use LZ4 crypto compressor"
    This reverts commit 02d95b2e0ee7759ba09fff29aefc15a6e11c6b61.

--- mm/zcache.c
+++ mm/zcache.c
@@ -48,8 +48,12 @@ module_param_named(enabled, zcache_enabled, bool, 0);
 /*
  * Compressor to be used by zcache
  */
-#define ZCACHE_COMPRESSOR_DEFAULT "lz4"
+#define ZCACHE_COMPRESSOR_DEFAULT "lzo"
+#ifndef CONFIG_CRYPTO_LZ4
 static char *zcache_compressor = ZCACHE_COMPRESSOR_DEFAULT;
+#else
+static char *zcache_compressor = "lz4";
+#endif
 module_param_named(compressor, zcache_compressor, charp, 0);
 
 /*
