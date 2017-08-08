63a5df150fec6993fa37ab38289caf1787858aab
MOVZX <movzx@yahoo.com>
2017-08-09 01:44:54 +0300

    Revert "Dynamic FSync: Use sys_sync to force sync when system panic or power down"
    This reverts commit b3a08a4150dc141d886454557c99f59ba1db0859.
    
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- fs/dyn_sync_cntrl.c
+++ fs/dyn_sync_cntrl.c
@@ -16,7 +16,6 @@
 #include <linux/writeback.h>
 #include <linux/dyn_sync_cntrl.h>
 #include <linux/lcd_notify.h>
-#include <linux/syscalls.h>
 
 // fsync_mutex protects dyn_fsync_active during suspend / late resume transitions
 static DEFINE_MUTEX(fsync_mutex);
@@ -95,7 +94,7 @@ static int dyn_fsync_panic_event(struct notifier_block *this,
 		unsigned long event, void *ptr)
 {
 	suspend_active = false;
-	sys_sync();
+	dyn_fsync_force_flush();
 	pr_warn("dynamic fsync: panic - force flush!\n");
 
 	return NOTIFY_DONE;
@@ -108,7 +107,7 @@ static int dyn_fsync_notify_sys(struct notifier_block *this, unsigned long code,
 	if (code == SYS_DOWN || code == SYS_HALT) 
 	{
 		suspend_active = false;
-		sys_sync();
+		dyn_fsync_force_flush();
 		pr_warn("dynamic fsync: reboot - force flush!\n");
 	}
 	return NOTIFY_DONE;
