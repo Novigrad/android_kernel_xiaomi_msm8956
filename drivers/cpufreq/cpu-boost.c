c2843043f329704cfabeb2b30e5c212a6a99a867
TheCrazyLex <alex.naidis@linux.com>
2017-08-22 02:45:33 +0300

    sched: Introduce Shadow scheduling
    Shadow scheduling is an extension for the
    HMP scheduler which provides efficient hotswitching
    between two scheduling policies.
    
    [...] (The documentation for this feature needs
           to be left out since it is property of
           Nextbit Systems Inc.)
    
    Configuration:
    
    sched_use_shadow_scheduling - controls whether shadow
                                  scheduling is enabled
    
    sched_shadow_active - internal variable for tracking
                          the shadow activity state
    
    [...]
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -17,11 +17,13 @@
 #include <linux/init.h>
 #include <linux/cpufreq.h>
 #include <linux/cpu.h>
+#include <linux/kthread.h>
 #include <linux/sched.h>
 #include <linux/moduleparam.h>
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
+#include <linux/sched/rt.h>
 
 struct cpu_sync {
 	int cpu;
@@ -30,9 +32,8 @@ struct cpu_sync {
 };
 
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
-static struct workqueue_struct *cpu_boost_wq;
 
-static struct work_struct input_boost_work;
+static struct kthread_work input_boost_work;
 
 static unsigned int input_boost_enabled = 1;
 module_param(input_boost_enabled, uint, 0644);
@@ -47,6 +48,9 @@ static bool sched_boost_active;
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
+static bool input_boost_pending;;
+static struct kthread_worker cpu_boost_worker;
+static struct task_struct *cpu_boost_worker_thread;
 #define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
 
 static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
@@ -179,6 +183,11 @@ static void do_input_boost_rem(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
+	if (input_boost_pending)
+		return;
+
+	sched_set_shadow_active(false);
+
 	if (sched_boost_active) {
 		ret = sched_set_boost(0);
 		if (ret)
@@ -187,13 +196,15 @@ static void do_input_boost_rem(struct work_struct *work)
 	}
 }
 
-static void do_input_boost(struct work_struct *work)
+static void do_input_boost(struct kthread_work *work)
 {
 	unsigned int i, ret;
 	struct cpu_sync *i_sync_info;
 
 	if (!input_boost_ms)
 		return;
+
+	input_boost_pending = true;
 
 	cancel_delayed_work_sync(&input_boost_rem);
 	if (sched_boost_active) {
@@ -211,6 +222,10 @@ static void do_input_boost(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
+	input_boost_pending = false;
+
+	sched_set_shadow_active(true);
+
 	/* Enable scheduler boost to migrate tasks to big cluster */
 	if (sched_boost_on_input) {
 		ret = sched_set_boost(1);
@@ -222,8 +237,7 @@ static void do_input_boost(struct work_struct *work)
 		}
 	}
 
-	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
-					msecs_to_jiffies(input_boost_ms));
+	schedule_delayed_work(&input_boost_rem, msecs_to_jiffies(input_boost_ms));
 }
 
 static void cpuboost_input_event(struct input_handle *handle,
@@ -238,10 +252,10 @@ static void cpuboost_input_event(struct input_handle *handle,
 	if (now - last_input_time < (input_boost_ms * USEC_PER_MSEC))
 		return;
 
-	if (work_pending(&input_boost_work))
+	if (queuing_blocked(&cpu_boost_worker, &input_boost_work))
 		return;
 
-	queue_work(cpu_boost_wq, &input_boost_work);
+	queue_kthread_work(&cpu_boost_worker, &input_boost_work);
 	last_input_time = ktime_to_us(ktime_get());
 }
 
@@ -321,14 +335,17 @@ static int cpu_boost_init(void)
 	int cpu, ret;
 	struct cpu_sync *s;
 
-	cpu_boost_wq =
-	    alloc_workqueue("cpuboost_wq",
-			    WQ_HIGHPRI | WQ_UNBOUND, 0);
+	struct sched_param param = { .sched_priority = MAX_RT_PRIO - 2 };
 
-	if (!cpu_boost_wq)
+	init_kthread_worker(&cpu_boost_worker);
+	cpu_boost_worker_thread = kthread_run(kthread_worker_fn,
+		&cpu_boost_worker, "cpu_boost_worker_thread");
+	if (IS_ERR(cpu_boost_worker_thread))
 		return -EFAULT;
 
-	INIT_WORK(&input_boost_work, do_input_boost);
+	sched_setscheduler(cpu_boost_worker_thread, SCHED_FIFO, &param);
+
+	init_kthread_work(&input_boost_work, do_input_boost);
 	INIT_DELAYED_WORK(&input_boost_rem, do_input_boost_rem);
 
 	for_each_possible_cpu(cpu) {
